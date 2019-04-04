92ffbc28e1b52ef39e4e007fa716bf6aab0fe9bd
Molly Rossow <mrossow@chapinhall.org>
2019-04-04 14:54:39 -0500

    Move metabase updates into single transaction

--- metabase/extract_metadata.py
+++ metabase/extract_metadata.py
@@ -21,29 +21,29 @@ class ExtractMetadata():
         """
         self.data_table_id = data_table_id
 
-        self.metabase_conn = psycopg2.connect(
-            settings.metabase_connection_string)
-        self.metabase_conn.autocommit = True
-        self.metabase_cur = self.metabase_conn.cursor()
+        self.metabase_connection_string = settings.metabase_connection_string
 
         self.data_conn = psycopg2.connect(settings.data_connection_string)
         self.data_conn.autocommit = True
         self.data_cur = self.data_conn.cursor()
 
-        self.schema_name, self.table_name = self.__get_table_name()
-
     def process_table(self, categorical_threshold=10):
         """Update the metabase with metadata from this Data Table."""
 
-        self._get_table_level_metadata()
-        self._get_column_level_metadata(categorical_threshold)
+        with psycopg2.connect(self.metabase_connection_string) as conn:
+            with conn.cursor() as cursor:
+                schema_name, table_name = self.__get_table_name(cursor)
+                self._get_table_level_metadata(cursor, schema_name, table_name)
+                self._get_column_level_metadata(
+                    cursor,
+                    schema_name,
+                    table_name,
+                    categorical_threshold)
 
-        self.metabase_cur.close()
-        self.metabase_conn.close()
         self.data_cur.close()
         self.data_conn.close()
 
-    def _get_table_level_metadata(self):
+    def _get_table_level_metadata(self, metabase_cur, schema_name, table_name):
         """Extract table level metadata and store it in the metabase.
 
         Extract table level metadata (number of rows, number of columns and
@@ -55,8 +55,8 @@ class ExtractMetadata():
         """
         self.data_cur.execute(
             sql.SQL('SELECT COUNT(*) as n_rows FROM {}.{};').format(
-                sql.Identifier(self.schema_name),
-                sql.Identifier(self.table_name),
+                sql.Identifier(schema_name),
+                sql.Identifier(table_name),
             )
         )
         n_rows = self.data_cur.fetchone()[0]
@@ -69,13 +69,13 @@ class ExtractMetadata():
                     TABLE_SCHEMA = %s
                     AND TABLE_NAME = %s
             """),
-            [self.schema_name, self.table_name]
+            [schema_name, table_name]
         )
         n_cols = self.data_cur.fetchone()[0]
 
         self.data_cur.execute(
             sql.SQL('SELECT PG_RELATION_SIZE(%s);'),
-            [self.schema_name + '.' + self.table_name],
+            [schema_name + '.' + table_name],
         )
         table_size = self.data_cur.fetchone()[0]
 
@@ -83,7 +83,7 @@ class ExtractMetadata():
             raise ValueError('Selected data table has 0 rows.')
             # This will also capture n_cols == 0 and size == 0.
 
-        self.metabase_cur.execute(
+        metabase_cur.execute(
             """
                 UPDATE metabase.data_table
                 SET
@@ -107,7 +107,8 @@ class ExtractMetadata():
         # TODO: Update create_by and date_created
         # https://github.com/chapinhall/adrf-metabase/pull/8#discussion_r265339190
 
-    def _get_column_level_metadata(self, categorical_threshold):
+    def _get_column_level_metadata(self, metabase_cur, schema_name, table_name,
+                                   categorical_threshold):
         """Extract column level metadata and store it in the metabase.
 
         Process columns one by one, identify or infer type, update Column Info
@@ -115,23 +116,36 @@ class ExtractMetadata():
 
         """
 
-        column_names = self.__get_column_names()
+        column_names = self.__get_column_names(schema_name, table_name)
 
         for col_name in column_names:
-            column_data = self.__get_column_type(col_name,
+            column_data = self.__get_column_type(schema_name,
+                                                 table_name,
+                                                 col_name,
                                                  categorical_threshold)
             if column_data.type == 'numeric':
-                self.__update_numeric_metadata(col_name, column_data.data)
+                self.__update_numeric_metadata(
+                    metabase_cur,
+                    col_name, column_data.data)
             elif column_data.type == 'text':
-                self.__update_text_metadata(col_name, column_data.data)
+                self.__update_text_metadata(
+                    metabase_cur,
+                    col_name,
+                    column_data.data)
             elif column_data.type == 'date':
-                self.__update_date_metadata(col_name, column_data.data)
+                self.__update_date_metadata(
+                    metabase_cur,
+                    col_name,
+                    column_data.data)
             elif column_data.type == 'code':
-                self.__update_code_metadata(col_name, column_data.data)
+                self.__update_code_metadata(
+                    metabase_cur,
+                    col_name,
+                    column_data.data)
             else:
                 raise ValueError('Unknown column type')
 
-    def __get_column_names(self):
+    def __get_column_names(self, schema_name, table_name):
         """Returns the names of the columns in the data table.
 
         Returns:
@@ -145,15 +159,15 @@ class ExtractMetadata():
                 AND table_name  = %(table)s;
                 """,
                 {
-                    'schema': self.schema_name,
-                    'table': self.table_name
+                    'schema': schema_name,
+                    'table': table_name
                 },
                 )
 
         columns = self.data_cur.fetchall()
         return([c[0] for c in columns])
 
-    def __get_table_name(self):
+    def __get_table_name(self, metabase_cur):
         """Return the the table schema and name using the Data Table ID.
 
         Returns table name and schema name by looking up the Data Table ID in
@@ -164,7 +178,7 @@ class ExtractMetadata():
             (str, str): (schema name, table name)
 
         """
-        self.metabase_cur.execute(
+        metabase_cur.execute(
             """
             SELECT file_table_name
             FROM metabase.data_table
@@ -173,7 +187,7 @@ class ExtractMetadata():
             {'data_table_id': self.data_table_id},
         )
 
-        result = self.metabase_cur.fetchone()
+        result = metabase_cur.fetchone()
 
         if result is None:
             raise ValueError('data_table_id not found in metabase.data_table')
@@ -185,7 +199,8 @@ class ExtractMetadata():
 
         return schema_name_table_name_tp
 
-    def __get_column_type(self, col, categorical_threshold):
+    def __get_column_type(self, schema_name, table_name, col,
+                          categorical_threshold):
         """Identify or infer column type.
 
         Infers the column type.
@@ -199,13 +214,13 @@ class ExtractMetadata():
             self.data_cur,
             col,
             categorical_threshold,
-            self.schema_name,
-            self.table_name
+            schema_name,
+            table_name
         )
 
         return column_data
 
-    def __update_numeric_metadata(self, col_name, col_data):
+    def __update_numeric_metadata(self, metabase_cur, col_name, col_data):
         """Extract metadata from a numeric column.
 
         Extract metadata from a numeric column and store metadata in Column
@@ -214,13 +229,13 @@ class ExtractMetadata():
         """
 
         extract_metadata_helper.update_numeric(
-            self.metabase_cur,
+            metabase_cur,
             col_name,
             col_data,
             self.data_table_id,
         )
 
-    def __update_text_metadata(self, col_name, col_data):
+    def __update_text_metadata(self, metabase_cur, col_name, col_data):
         """Extract metadata from a text column.
 
         Extract metadata from a text column and store metadata in Column Info
@@ -229,13 +244,13 @@ class ExtractMetadata():
         """
 
         extract_metadata_helper.update_text(
-            self.metabase_cur,
+            metabase_cur,
             col_name,
             col_data,
             self.data_table_id,
         )
 
-    def __update_date_metadata(self, col_name, col_data):
+    def __update_date_metadata(self, metabase_cur, col_name, col_data):
         """Extract metadata from a date column.
 
         Extract metadata from date column and store metadate in Column Info and
@@ -244,13 +259,13 @@ class ExtractMetadata():
         """
 
         extract_metadata_helper.update_date(
-            self.metabase_cur,
+            metabase_cur,
             col_name,
             col_data,
             self.data_table_id,
         )
 
-    def __update_code_metadata(self, col_name, col_data):
+    def __update_code_metadata(self, metabase_cur, col_name, col_data):
         """Extract metadata from a categorial column.
 
         Extract metadata from a categorial columns and store metadata in Column
@@ -260,7 +275,7 @@ class ExtractMetadata():
         # TODO: modify categorical_threshold to take percentage arguments.
 
         extract_metadata_helper.update_code(
-            self.metabase_cur,
+            metabase_cur,
             col_name,
             col_data,
             self.data_table_id,
